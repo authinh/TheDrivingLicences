f858a361ac25df0857b9079d6e146a79a123dc76
authinh <auvanthinh@hotmail.com>
2017-03-04 16:33:06 +0700

    finish gui login

---
+++ TheDrivingLicencesClient/DAL/AnswersDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDrivingLicencesClient.DAL
+{
+    public class AnswersDAL
+    {
+        private DataClasses1DataContext db;
+
+        public AnswersDAL()
+        {
+            db = new DataClasses1DataContext();
+        }
+        /**
+         * param name="QuestionID"
+         * param name="ans" là chuỗi các đáp án đúng
+         * một câu hỏi có nhiều đáp án đúng
+         * returns true false
+         */
+        public bool checkAnswer(int QuestionID,string ans){
+            var listAnswer = from table in db.Answers
+                             where table.QuestionID == QuestionID
+                             select table.AnswerTrue;
+            foreach (int answers in listAnswer)
+            {
+                if (!ans.Contains(answers+""))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TheDrivingLicencesClient/DAL/UserDAL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDrivingLicencesClient.DAL
+{
+    public class UserDAL
+    {
+        private DataClasses1DataContext db;
+
+        public UserDAL()
+        {
+            db = new DataClasses1DataContext();
+        }
+        /**
+         *param name="user"
+         *param name="user"
+         * returns id >=0
+         * returns -1 if user pass incorrect
+         * returns -2 other error
+         */
+        public int checkAccount(string user, string pass)
+        {
+            try {
+                int userId = (from table in db.Users
+                              where (table.Username.Equals(user) && table.Password.Equals(pass))  
+                                select table.UserID).SingleOrDefault();
+
+                if (userId > 0){
+                    return userId;
+                }
+                 else {
+                    return -1;
+                }
+                }
+                catch(InvalidOperationException){
+                    return -2;
+                }
+        }
+
+        /**
+         * param name="userId"
+         * returns info use
+         */
+        public User getInfoUser(int userId)
+        {
+            try
+            {
+                User user = (from table in db.Users
+                             where (table.UserID == userId)
+                             select table
+                                ).SingleOrDefault();
+
+                return user;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+
+        }
+
+    }
+}
